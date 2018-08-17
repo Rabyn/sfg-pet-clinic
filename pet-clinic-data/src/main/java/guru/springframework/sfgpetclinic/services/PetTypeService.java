b91bebf75a19c30be055f522f5a570cdc9705738
rabyn <rabynshrestha@gmail.com>
2018-08-17 16:52:17 -0500

    Create PetType Map Service Impl. Closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+/*
+ * Created by Rabindra on 8/17/18
+ */
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
