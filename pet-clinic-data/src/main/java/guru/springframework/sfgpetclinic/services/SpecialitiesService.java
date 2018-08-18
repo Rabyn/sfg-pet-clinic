b0c49a6e0543cf56b39a59429ac394ed08849778
rabyn <rabynshrestha@gmail.com>
2018-08-17 23:24:09 -0500

    Create Speciality Map Service Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+/*
+ * Created by Rabindra on 8/17/18
+ */
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
